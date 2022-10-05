c97e8c891b44395794f9039d653cf34255c8591d
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-10-05 12:35:54 +0300

    feat: make datafetching async

--- statistics.js
+++ statistics.js
@@ -1,42 +1,51 @@
+window.loadData();
+
 let statisticsData;
 const pending = [];
 const approved = [];
 const rejected = [];
 
-fetch("./Data/data.json")
-  .then((res) => res.json())
-  .then((dataObject) => {
-    statisticsData = dataObject.data;
-    const quantityOfTimesheets = statisticsData.length;
+async function loadData() {
+  fetch("./Data/data.json")
+    .then((res) => res.json())
+    .then((dataObject) => {
+      statisticsData = dataObject.data;
+      const quantityOfTimesheets = statisticsData.length;
 
-    document.getElementById("submitted").innerHTML = statisticsData.length;
+      document.getElementById("submitted").innerHTML = statisticsData.length;
 
-    console.log(statisticsData);
-    statisticsData.forEach((item) => {
-      if (item.Status === "Approved") {
-        approved.push(item);
+      statisticsData.forEach((item) => {
+        if (item.Status === "Approved") {
+          approved.push(item);
 
-        percentage = Math.floor((approved.length / quantityOfTimesheets) * 100);
+          percentage = Math.floor(
+            (approved.length / quantityOfTimesheets) * 100
+          );
 
-        document.getElementById("approved").innerHTML = approved.length;
+          document.getElementById("approved").innerHTML = approved.length;
 
-        document.getElementById("approvedperc").innerHTML = `${percentage}%`;
-      } else if (item.Status === "Rejected") {
-        rejected.push(item);
+          document.getElementById("approvedperc").innerHTML = `${percentage}%`;
+        } else if (item.Status === "Rejected") {
+          rejected.push(item);
 
-        percentage = Math.floor((rejected.length / quantityOfTimesheets) * 100);
+          percentage = Math.floor(
+            (rejected.length / quantityOfTimesheets) * 100
+          );
 
-        document.getElementById("rejected").innerHTML = rejected.length;
+          document.getElementById("rejected").innerHTML = rejected.length;
 
-        document.getElementById("rejectedperc").innerHTML = `${percentage}%`;
-      } else if (item.Status === "Pending") {
-        pending.push(item);
+          document.getElementById("rejectedperc").innerHTML = `${percentage}%`;
+        } else if (item.Status === "Pending") {
+          pending.push(item);
 
-        percentage = Math.floor((pending.length / quantityOfTimesheets) * 100);
+          percentage = Math.floor(
+            (pending.length / quantityOfTimesheets) * 100
+          );
 
-        document.getElementById("pending").innerHTML = pending.length;
+          document.getElementById("pending").innerHTML = pending.length;
 
-        document.getElementById("pendingperc").innerHTML = `${percentage}%`;
-      }
+          document.getElementById("pendingperc").innerHTML = `${percentage}%`;
+        }
+      });
     });
-  });
+}
