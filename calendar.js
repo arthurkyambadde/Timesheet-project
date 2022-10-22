5a6b978731924bae49692b9b151704fd9bcce1ef
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-10-23 02:17:47 +0300

    feat: change from mobiscroll calendar

--- calendar.js
+++ calendar.js
@@ -1,12 +1,27 @@
-mobiscroll.setOptions({
-  // theme: "ios",
-  themeVariant: "light",
-});
+// mobiscroll.setOptions({
+//   // theme: "ios",
+//   themeVariant: "light",
+// });
 
-mobiscroll.datepicker("#demo-week-select", {
-  controls: ["calendar"],
-  select: "preset-range",
-  display: "inline",
-  firstSelectDay: 0,
-  selectSize: 7,
-});
+// mobiscroll.datepicker("#demo-week-select", {
+//   controls: ["calendar"],
+//   select: "preset-range",
+//   display: "inline",
+//   firstSelectDay: 0,
+//   selectSize: 7,
+// });
+
+// $(document).ready(function () {
+//   // Add weekpicker
+//   $("#Week").datepicker({
+//     calendarWeeks: true,
+//     weekStart: 1,
+//   });
+// });
+
+// $("#Week").on("changeDate", function (e) {
+//   var value = $("#Week").val();
+//   var year = moment().format("YYYY");
+//   var week = moment(e.date).format("WW");
+//   $("#Week").val(year + "-W" + week);
+// });
